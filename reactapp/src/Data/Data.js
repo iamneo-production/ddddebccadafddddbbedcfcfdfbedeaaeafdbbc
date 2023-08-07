f3cb26ed342a2ad637ef7f571c0826ca3ca7d4e0
Iamneo <techadmin@iamneo.ai>
2023-08-07 12:53:30 +0000

    Backup Commit

---
+++ reactapp/src/Data/Data.js
@@ -0,0 +1,38 @@
+const QuizData = [{
+    Question: 'Who is the father of nation ?',
+    options: [
+        'Mahatma Gandhi', 'Jawaharlal Nehru', 'Donald Trump', 'Barrack Obama'
+    ],
+    correctAnswer: 'Mahatma Gandhi'
+},
+{
+    Question: 'What color is are the leaves ?',
+    options: [
+        'Blue', 'Red', 'Yellow', 'Green'
+    ],
+    correctAnswer: 'Green'
+},
+{
+    Question: 'What color is the sky ?',
+    options: [
+        'Blue', 'Red', 'Yellow', 'Green'
+    ],
+    correctAnswer: 'Blue'
+},
+{
+    Question: 'What color is the sky ?',
+    options: [
+        'Blue', 'Red', 'Yellow', 'Green'
+    ],
+    correctAnswer: 'Blue'
+},
+{
+    Question: 'What color is the fire ?',
+    options: [
+        'Blue', 'Red', 'Yellow', 'Green'
+    ],
+    correctAnswer: 'Red'
+}
+
+]
+export default QuizData;
